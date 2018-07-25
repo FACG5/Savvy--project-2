673571d71e83f86b1a827a28816911d22ecdb02a
ahmad1091 <ahmadtayeb91@gmail.com>
2018-07-25 12:03:36 +0300

    updated logic and wrote tests

---
+++ src/accessData.js
@@ -0,0 +1,15 @@
+const DataName = require("./DataName.json");
+
+function getData(data) {
+  var arr = [];
+  for (let ele of DataName) {
+    console.log(ele)
+    if (ele.toLowerCase().startsWith(data.wordAutoComplete.toLowerCase()))
+      arr.push(ele);
+    if (arr.length == 10)
+      break;
+  }
+  return arr;
+}
+
+module.exports = getData;
